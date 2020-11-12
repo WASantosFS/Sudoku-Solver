3268c67e622fb1b68890ec5a8dbfb53a829f8003
William A. Santos <baekgu@live.com>
2020-11-12 08:33:21 -0500

    Started work on the solver.
    Working on visual printout of the puzzle for the user to confirm what they gave as input is correct.

---
+++ Sudoku-Solver/Sudoku-Solver/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Sudoku_Solver;
+
+namespace Sudoku_Solver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserInterface userInterface = new UserInterface();
+
+            userInterface.Start();
+        }
+    }
+}
---
+++ Sudoku-Solver/Sudoku-Solver/UserInterface.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sudoku_Solver
+{
+    public class UserInterface
+    {
+        /// <summary>
+        /// Simple start-up method.
+        /// </summary>
+        public void Start()
+        {
+            Console.WriteLine("Hello! Welcome to Silly Sally's Sudoku Solver!");
+            Console.WriteLine("(Sorry, I couldn't help myself...)");
+            Console.WriteLine();
+            Console.WriteLine("Please input the sudoku you wish to be solved, replacing blanks with 0s.");
+            string userInput = Console.ReadLine();
+
+
+        }
+
+        /// <summary>
+        /// Confirmation that what was input is correct.
+        /// </summary>
+        public bool Confirm(string sudoku)
+        {
+            BuildVisualSudoku(sudoku);
+
+            Console.WriteLine("Is what you see correct?");
+
+            return true;
+        }
+
+        /// <summary>
+        /// Builds a visual representation of the sudoku.
+        /// </summary>
+        /// <param name="sudoku"></param>
+        public void BuildVisualSudoku(string sudoku)
+        {
+            List<string> sudokuList = sudoku.Split(' ').ToList();
+
+
+        }
+    }
+}
